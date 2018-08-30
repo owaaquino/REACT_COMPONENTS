82e0583ec4c185131053331c27bd1cc76e3881fb
owaaquino <josejoshuaaquino@gmail.com>
2018-08-30 19:18:47 +0800

    Add a decrement increment component

--- toggle_button/src/App.js
+++ toggle_button/src/App.js
@@ -3,12 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import ToggleBtn_simple from "./components/ToggleBtn_simple";
 import ToggleBtn_colorChange from "./components/ToggleBtn_colorChange";
+import Counter from "./components/Counter";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <ToggleBtn_simple />
         <ToggleBtn_colorChange />
+        <Counter />
       </div>
     );
   }
---
+++ toggle_button/src/components/Counter.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+class Counter extends React.Component {
+  state = {
+    counter: 0
+  };
+
+  addOne = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
+
+  subOne = () => {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  };
+
+  reset = () => {
+    this.setState({
+      counter: 0
+    });
+  };
+  render() {
+    return (
+      <div>
+        <div>
+          Click the button bellow to update our counter: {this.state.counter}
+        </div>
+        <button onClick={this.addOne}>Add 1</button>
+        <button onClick={this.subOne}>Subtract 1</button>
+        <button onClick={this.reset}>Reset</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
