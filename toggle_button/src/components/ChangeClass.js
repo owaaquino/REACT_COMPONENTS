e0374fc7d425292fcc9029030afb81887a2c6fe5
owaaquino <josejoshuaaquino@gmail.com>
2018-09-01 23:21:01 +0800

    Change Class on click

---
+++ toggle_button/src/components/ChangeClass.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class ChangeClass extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="boxDiv" />
+        <button>Change</button>
+      </div>
+    );
+  }
+}
+
+export default ChangeClass;
