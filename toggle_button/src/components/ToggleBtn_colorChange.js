80ea88a1d1092e79c9cfff0b01d831c64d5b88bb
owaaquino <josejoshuaaquino@gmail.com>
2018-09-03 18:05:51 +0800

    change color text update + css classes

--- toggle_button/src/components/ToggleBtn_colorChange.js
+++ toggle_button/src/components/ToggleBtn_colorChange.js
@@ -1,19 +1,28 @@
 import React from "react";
 
 class ToggleBtn_colorChange extends React.Component {
+  state = {
+    spanColor: "colorText"
+  };
+
+  changeColor = event => {
+    this.setState({
+      spanColor: event.target.textContent
+    });
+  };
 
   render() {
     return (
       <div>
         <h2>Colors changes</h2>
         <p>
-          <span className="colorText">Color</span> changes on click of the
-          button.
+          <span className={this.state.spanColor}>Color</span> changes on click
+          of the button.
         </p>
         <div className="color_box" />
-        <button>red</button>
-        <button>blue</button>
-        <button>green</button>
+        <button onClick={this.changeColor}>red</button>
+        <button onClick={this.changeColor}>blue</button>
+        <button onClick={this.changeColor}>green</button>
       </div>
     );
   }
